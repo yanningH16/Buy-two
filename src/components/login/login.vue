525c9c01f3dfde7f2ecbad91ed0a4f849bc1da7d
yanglx <1071126434@qq.com>
2017-11-14 11:30:43 +0800

    代码初始化

---
+++ src/components/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    login cont
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'login',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+</style>
