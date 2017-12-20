ed53135d42c13a14387bb02058eeb4766e998343
yanglx <1071126434@qq.com>
2017-12-20 20:02:50 +0800

    跳转

--- src/components/evalute/evalute.vue
+++ src/components/evalute/evalute.vue
@@ -123,7 +123,7 @@ export default {
             Toast({
               message: '提交成功!'
             })
-            this.$router.push({ name: 'userCenter' })
+            this.$router.push({ name: 'myTask' })
           } else {
             Toast({
               message: data.data.message
