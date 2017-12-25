eabbc26f3956e70f9fd4776c78a5745611bc8325
yanglx <1071126434@qq.com>
2017-12-25 19:18:50 +0800

    port

--- buyer.js
+++ buyer.js
@@ -18,7 +18,7 @@ app.use(history({
 app.use(express.static('./dist'))
   // var port = process.env.PORT || config.build.port
   // 生产环境的端口号
-var port = 80
+var port = 8012
 module.exports = app.listen(port, function(err) {
   if (err) {
     console.log(err)
